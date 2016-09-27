c08f9b2f363a5d2bff5dd400909c25005ebaf0cb
mkamenet3 <maria.kamenetsky@gmail.com>
2016-09-27 15:00:32 -0500

    changed file structure to hw1 directory and changed relative file path to reflect this in script

--- hw1/scripts/normalizeFileNames.sh
+++ hw1/scripts/normalizeFileNames.sh
@@ -1,12 +1,13 @@
 #This shell scropt will change the file names from timetesty_snaw.log to
 #timetest0y_snaq.log, where "y" is a digit between 1 and 9.
 #The .out files will be changed similarly as well.
+#This script should be run from the hw1 directory
 
 
 
 
 #1) Change .log files to include '0' before single digit
-for logfile in log/timetest?_snaq.log
+for logfile in data/log/timetest?_snaq.log
  do mv "$logfile" "${logfile/est/est0}"
 done
 
@@ -15,7 +16,7 @@ done
 
 #2) Change .out files to include '0' before single digit  
 
-for outfile in out/timetest?_snaq.out
+for outfile in data/out/timetest?_snaq.out
   do mv "$outfile" "${outfile/est/est0}"
 done
 
